f982fc1a8af7baf7c44b3fd2a2d97e149ba3d3ff
jaindy <jaindy@mail.uc.edu>
2024-02-05 18:11:24 -0500

    echo.php revision: added input field validation and encoded the input

--- labs/lab1/echo.php
+++ labs/lab1/echo.php
@@ -1,3 +1,10 @@
 <?php
-	echo $_REQUEST["data"];
+
+   
+ if(empty($_REQUEST["data"])) {
+    echo "Input is required";
+  } else {
+    echo htmlentities($_REQUEST["data"],ENT_QUOTES, "UTF-8");
+  }
+
 ?>
